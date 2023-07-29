b34e1c6b705d7d3dc755e132cf39932962c81624
TAkayaSakura <sakutaka0523@outlook.jp>
2023-07-30 00:21:29 +0900

    kansei

--- pw_myown2.mjs
+++ pw_myown2.mjs
@@ -4,50 +4,40 @@ const { chromium } = require('playwright');
 
 
 (async () => {
-    const sample1 = "あ"//被監査部局〇〇部
-    const sample2 = "い"//〇〇課
-    const sample3 = "う"//システム管理者名
-    const sample4 = "え"//システム名
-    const sample5 = "お"//文書番号
-    const sample6 = "か"//記入日
-    const sample7 = "き"//記入担当者
 
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto('https://www.grove.kutc.kansai-u.ac.jp/takatsuki/test.html');
 
     for (let i = 1; i <= 7; i++) {
-
         const inputformpass = `body > table:nth-child(4) > tbody > tr:nth-child(${i}) > td > input[type=textbox]`
-
-        const inputformvariable = `sample${i}`
-
+        const inputformvariable = `input${i}`
         const inputform = eval(inputformvariable);
-
         await page.locator(inputformpass).fill(inputform);
     }
-
-
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(1) > td > input[type=textbox]').fill(sample1);
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(2) > td > input[type=textbox]').fill(sample2);
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(3) > td > input[type=textbox]').fill(sample3);
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(4) > td > input[type=textbox]').fill(sample4);
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(5) > td > input[type=textbox]').fill(sample5);
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(6) > td > input[type=textbox]').fill(sample6);
-    // await page.locator('body > table:nth-child(4) > tbody > tr:nth-child(7) > td > input[type=textbox]').fill(sample7);
 
     for (let i = 2; i <= 57; i++) {
         const selector = `body > table:nth-child(5) > tbody > tr:nth-child(${i}) > td:nth-child(3) > select`;
         await page.selectOption(selector, "YES");
-      }
+    }
 
-    // await page.selectOption("body > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(3) > select", "YES");
-    // await page.selectOption("body > table:nth-child(5) > tbody > tr:nth-child(3) > td:nth-child(3) > select", "YES");
-    // await page.selectOption("body > table:nth-child(5) > tbody > tr:nth-child(4) > td:nth-child(3) > select", "YES");
-    // await page.selectOption("body > table:nth-child(5) > tbody > tr:nth-child(5) > td:nth-child(3) > select", "YES");
-    // await page.selectOption("body > table:nth-child(5) > tbody > tr:nth-child(6) > td:nth-child(3) > select", "YES");
-    // await page.selectOption("body > table:nth-child(5) > tbody > tr:nth-child(7) > td:nth-child(3) > select", "YES");...
-    
-    await page.screenshot({path: './test.png', fullPage: true });
+    await page.screenshot({path: './test.png', fullPage: true }); //ページ全体のスクリーンショットを撮影します（必要に応じてコメントアウトしてください）
+
+    await page.locator('body > form > input[type=submit]').click(); //送信ボタンをクリックします（必要に応じてコメントアウトしてください）
+
     await browser.close();
+
 })();
+
+const input1 = "a"//被監査部局〇〇部
+const input2 = "b"//〇〇課
+const input3 = "c"//システム管理者名
+const input4 = "d"//システム名
+const input5 = "e"//文書番号
+const input6 = "f"//記入日
+const input7 = "g"//記入担当者
+
+///////////////////////////////////////////////////////////
+//主に24行目，26行目，32~38行目を必要に応じて操作してください//
+//他の行において変更が必要である場合は他の行も操作してください//
+//////////////////////////////////////////////////////////
